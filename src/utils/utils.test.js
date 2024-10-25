7adcfbe0d1e7ec530ca79a4af69363caf728443f
marimunari <marianamunari4@gmail.com>
2024-10-25 20:47:00 -0300

    feat: implementing new features

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,29 +1,31 @@
 import { calculaNovoSaldo } from './index';
 
 describe('Quando eu realizo uma transação', () => {
-  test('que é um depósito, o saldo deve aumentar', () => {
+  it('Que é um depósito, o saldo deve aumentar', () => {
     const transacao = {
       transacao: 'Depósito',
-      valor: 50
-    }
+      valor: 50,
+    };
 
     const novoSaldo = calculaNovoSaldo(transacao, 100);
+
     expect(novoSaldo).toBe(150);
   });
 
-  test('que é um transferência, o saldo deve diminuir', () => {
+  test('Que é uma Transferência, o saldo deve diminuir', () => {
     const transacao = {
-      transacao: 'Transferência',
-      valor: 50
-    }
+      transacao: 'Tranferência',
+      valor: 50,
+    };
 
     const novoSaldo = calculaNovoSaldo(transacao, 100);
+
     expect(novoSaldo).toBe(50);
   });
 });
 
-test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
-  const calculaRendimento = jest.fn(saldo => saldo + saldo * 0.005);
+it('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+  const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
   const saldo = 100;
   const novoSaldo = calculaRendimento(saldo);
 
